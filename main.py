dfe4a74551f720ea166396dde4e539e45cef019c
tep98 <Korotkovstepan95@gmail.com>
2025-01-17 20:46:57 +0300

    feat: create state store system for save user action

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from os.path import split
+
 import telebot
 from datetime import datetime
 from telebot import types
 import openpyxl
 import os
+
+from telebot.apihelper import download_file
+from telebot.storage import StateMemoryStorage
+
+# Инициализация памяти для хранения состояний
+storage = StateMemoryStorage()
 
 #Создание временной директории для файлов
 TEMP_DIR = 'temp_files'
@@ -10,7 +18,7 @@ if not os.path.exists(TEMP_DIR):
     os.makedirs(TEMP_DIR)
 
 TOKEN = ""
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN, state_storage=storage)
 
 @bot.message_handler(commands=['start', 'restart'])
 def start(message):
@@ -25,12 +33,12 @@ def start(message):
     elif (current_time>=6) and (current_time < 12):
         welcome_word = 'Доброе утро'
 
-        # Создание кнопок для взаимодействия
+    #Создание кнопок для взаимодействия
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('1', callback_data='count_complited_classes'))
     markup.add(types.InlineKeyboardButton('2', callback_data='analyze_average_score'))
 
-    # Отправка сообщения
+    #Отправка сообщения
     bot.send_message(message.chat.id, f"{welcome_word} {message.from_user.first_name}!\n\nВыберете действие:\n1. Подсчитывать количество проведенных пар по всем дисциплинам у группы\n2. Поиск студентов с средним баллом ниже чем 3", reply_markup=markup)
 
 
@@ -39,14 +47,21 @@ def callback_message(callback):
     chat_id = callback.message.chat.id
 
     if callback.data == 'count_complited_classes':
+        bot.set_state(chat_id, "count_complited_classes")
         bot.send_message(chat_id, "Пришлите мне файл \"Расписание группы\" типа XLSX")
     elif callback.data == 'analyze_average_score':
+        bot.set_state(chat_id, "analyze_average_score")
         bot.send_message(chat_id, "Пришлите мне файл \"Отчет по студентам\" типа XLSX")
 
 
 @bot.message_handler(content_types='document')
 def handle_file(message):
     chat_id = message.chat.id
+    state = bot.get_state(chat_id)
+
+    if not state:
+        bot.send_message(chat_id, "Пожалуйста, выберите действие с помощью меню!")
+        return
 
     try:
         #Получение информации о файле
@@ -68,47 +83,53 @@ def handle_file(message):
                 workbook = openpyxl.load_workbook(file_path)
                 sheet = workbook.active
 
-                rows_values = list(sheet.values)
-                print(rows_values)
-                subjects_list = []
-                subjects_count_list = []
+                #Действие 1 (Задание 7)
+                if state == 'count_complited_classes':
+                    rows_values = list(sheet.values)
+                    print(rows_values)
+                    subjects_list = []
+                    subjects_count_list = []
 
-                # Логика для подсчета количества проведенных пар по всем дисциплинам
-                for row in rows_values[1:]:
-                    for value in row[3:]:
-                        if value is not None:
-                            value_str = str(value)
+                    #Логика для подсчета количества проведенных пар по всем дисциплинам
+                    for row in rows_values[1:]:
+                        for value in row[3:]:
+                            if value is not None:
+                                value_str = str(value)
 
-                            if value_str.split(" ", 1)[0] == "Предмет:":
-                                value_str_splitline = value_str.splitlines()[0]
+                                if value_str.split(" ", 1)[0] == "Предмет:":
+                                    value_str_splitline = value_str.splitlines()[0]
 
-                                if value_str_splitline not in subjects_list:
-                                    subjects_list.append(value_str_splitline)
-                                    subjects_count_list.append(1)
+                                    if value_str_splitline not in subjects_list:
+                                        subjects_list.append(value_str_splitline)
+                                        subjects_count_list.append(1)
 
-                                else:
-                                    index = subjects_list.index(value_str_splitline) if value_str_splitline in subjects_list else None
-                                    if index is not None:
-                                        if subjects_count_list[index] is not None:
-                                            subjects_count_list[index] += 1
+                                    else:
+                                        index = subjects_list.index(value_str_splitline) if value_str_splitline in subjects_list else None
+                                        if index is not None:
+                                            if subjects_count_list[index] is not None:
+                                                subjects_count_list[index] += 1
 
-                # Подсчет и вывод результатов
-                result = ""
-                for subject in range(len(subjects_list)):
-                    result += f"{subjects_list[subject].split(' ', 1)[1]} - занятий: {subjects_count_list[subject]}\n"
+                    #Подсчет и вывод результатов
+                    result = ""
+                    for subject in range(len(subjects_list)):
+                       result += f"{subjects_list[subject].split(' ', 1)[1]} - занятий: {subjects_count_list[subject]}\n"
 
-                if result == "":
-                    bot.reply_to(message, "Файл не содержит требуемой информации, убедитесь в том, что выбрали нужный документ")
-                    return
+                    if result == "":
+                        bot.reply_to(message, "Файл не содержит требуемой информации, убедитесь в том, что выбрали нужный документ")
+                        return
 
-                bot.send_message(chat_id, f"Группа: {sheet['A2'].value}\n\n{result}")
+                    bot.send_message(chat_id, f"Группа: {sheet['A2'].value}\n\n{result}")
+
+
+
+                #Действие 2 (Задание 5)
+                elif state == 'analyze_average_score':
+                    print("Анализ средних оценок ниже чем 3")
 
             except Exception as E:
                 bot.reply_to(message, f'Ошибка при попытке открытия файла, {E}')
         else:
             bot.reply_to(message, 'Пожалуйста отправьте файл в формате "XLSX"')
-
-        print('Файл принят!')
 
     except Exception as E:
         bot.reply_to(message, f'Произошла ошибка в работе бота, {E}')
@@ -116,6 +137,7 @@ def handle_file(message):
     finally:
         if os.path.exists(file_path):
             os.remove(file_path)
+
 
 print("Bot Started!")
 bot.polling(non_stop=True)
